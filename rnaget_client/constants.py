43f1a4ea89a0a219f7e78ba820f530bfa067b127
“Emilio <righiemilio@gmail.com>
2023-04-04 09:38:40 +0200

    First commit

---
+++ rnaget_client/constants.py
@@ -0,0 +1,19 @@
+PROJECTS='/projects'
+STUDIES='/studies'
+EXPRESSIONS='/expressions'
+CONTINUOUS='/continuous'
+TICKET='/ticket'
+FILTER='/filters'
+BYTES='/bytes'
+FORMATS='/formats'
+UNITS='/units'
+
+gtex = 'http://gtexportal.org/rnaget'
+encode = 'https://rnaget.encodeproject.org'
+
+FILTER_TYPES = {
+    'projects': PROJECTS+FILTER,
+    'studies':STUDIES+FILTER,
+    'expressions':EXPRESSIONS+FILTER,
+    'conitnuous':CONTINUOUS+FILTER
+}
